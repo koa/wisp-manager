32eab36cb84b3a6792b5b2879eb13386e6ae7a4c
Andreas König <andreas.koenig@berg-turbenthal.ch>
2014-08-29 10:16:33 +0200

    added pid-file

--- src/main/java/ch/bergturbenthal/wisp/manager/WispManager.java
+++ src/main/java/ch/bergturbenthal/wisp/manager/WispManager.java
@@ -4,6 +4,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.system.ApplicationPidListener;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,8 +14,12 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan
 @EnableAutoConfiguration
 public class WispManager {
+
 	public static void main(final String[] args) throws Exception {
 		SpringApplication.run(WispManager.class, args);
+		final SpringApplication springApplication = new SpringApplication(WispManager.class);
+		springApplication.addListeners(new ApplicationPidListener("wisp-manager.pid"));
+		springApplication.run(args);
 	}
 
 	@Bean
